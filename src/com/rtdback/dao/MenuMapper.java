33dd6bdf0891fe3301bd6782fb4711242268a884
wushuoyouting <15979865662@163.com>
2017-12-16 10:04:38 +0800

    servie

--- src/com/rtdback/dao/MenuMapper.java
+++ src/com/rtdback/dao/MenuMapper.java
@@ -1,7 +1,16 @@
 package com.rtdback.dao;
 
+import java.util.List;
 
-public class MenuMapper {
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository("menuMapper")
+public interface MenuMapper {
+
+	@Select("select rm.menuid from role_menu rm,role r,account_role ar WHERE ar.roleid = r.id and r.id = rm.roleid and ar.accountid = #{accountid}")
+	List<Integer> loadAccountMenu(@Param("accountid") Integer accountid);
+	
 	
 }
